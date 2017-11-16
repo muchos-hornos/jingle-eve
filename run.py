082a36d920ed8677d161217b2ad941033e36a4de
muchos <grbg.cllctr@gmail.com>
2017-11-15 18:50:20 -0800

    added server side oauth2 with flask sentinel for eve. requires running redis process on default redis port 6379

--- run.py
+++ run.py
@@ -1,11 +1,21 @@
 from eve import Eve
 from flask import send_from_directory
+from flask_sentinel import ResourceOwnerPasswordCredentials, oauth
+from oauth2 import BearerAuth
 
-app = Eve(__name__, static_folder="static")
+
+app = Eve(__name__, auth=BearerAuth, static_folder="static")
+ResourceOwnerPasswordCredentials(app)
+
+@app.route('/endpoint')
+@oauth.require_oauth()
+def restricted_access():
+    return "You made it through and accessed the protected resource!"
+
 
 @app.route('/')
 def index():
     return send_from_directory("static", 'base.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(ssl_context='adhoc')
